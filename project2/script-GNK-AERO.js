10e58dd0541622cbcc4e0d0cfaabd431df657409
Highendricecooker <108791147+ginanicolekang@users.noreply.github.com>
2023-12-08 21:36:47 -0500

    !

---
+++ project2/script-GNK-AERO.js
@@ -0,0 +1,30 @@
+function myFunction() {
+  var element = document.body;
+  element.classList.toggle("dark-mode");
+}
+
+
+
+// An index to track the contender image 
+var imageIndex = 1;
+var candleImage; 
+
+// An event callback for starting the interval
+function startInterval() {
+ setInterval(displayNextImage, 3000);
+}
+
+function displayNextImage() {
+  candleImage.src = "candle png/candle" + imageIndex.toString() + ".png";
+  if(imageIndex === (36)) {
+    imageIndex = 1;
+  }
+  else {
+    imageIndex = imageIndex + 1; // It can be replaced with imageIndex ++
+  }
+}
+
+window.onload=function() {
+  candleImage = document.getElementById("responsive");
+ startInterval();
+}
---
+++ project3/tutorial/main.js
@@ -0,0 +1,27 @@
+// function setup(){
+//     createCanvas(500,500);
+// }
+
+// function setup(){
+//     //     createCanvas(windowWidth,windowHeight);
+//     // }
+
+// --drawing--
+// function draw() {
+//     background(0);
+//     fill(255);
+//     beginShape();
+//         vertex(100,100)
+// }
+
+// function windowResized() {
+//     createCanvas(windowWidth, windowHeight);
+// }
+
+// // function draw() {
+//         var x = windowWidth * 0.5;
+//         var y = windowHeight * 0.5;
+// //     background(0);
+// //     fill(255);
+// //     circle(x,y,diameter);
+// // }
